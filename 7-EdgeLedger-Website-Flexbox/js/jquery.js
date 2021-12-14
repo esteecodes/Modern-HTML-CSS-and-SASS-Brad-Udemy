d2ab5b1187041336684377058f2a4fe0de42c55f
Esther White <64324417+esteecodes@users.noreply.github.com>
2021-12-14 22:57:06 +0200

    finishing touches

---
+++ 7-EdgeLedger-Website-Flexbox/js/jquery.js
@@ -0,0 +1,37 @@
+// Smooth Scrolling 
+
+/* BUG FIXED --- on original example .btn is included but it should NOT be included, 
+because the blog page link (Read Our Blog) also gets affected by .preventDefault 
+and the blog page doesn't open !!! */
+
+// $('#navbar a, .btn').on('click', function (e) {
+//   if (this.hash !== ""); {
+//     e.preventDefault();
+
+//     const hash = this.hash;
+
+//     $('html, body').animate (
+//       {
+//         scrollTop: $(hash).offset().top - 70,
+//       },
+//       800
+//     );
+//   }
+// });
+
+
+// Smooth Scrolling FIXED 
+$('#navbar a').on('click', function (e) {
+  if (this.hash !== ""); {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate (
+      {
+        scrollTop: $(hash).offset().top - 70,
+      },
+      800
+    );
+  }
+});
